547deac8befa98ded21d29dac4547509003a5b80
samisnotinsane <sameen11@hotmail.com>
2020-06-27 17:22:50 +0100

    Adding scrolling cards to bottom of map.

--- lib/src/screens/map_screen.dart
+++ lib/src/screens/map_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
   static const String id = '/map_screen';
@@ -7,11 +8,46 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  final items = List<String>.generate(10000, (i) => "Item $i");
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Map screen.'),
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+            initialCameraPosition: CameraPosition(
+              target: LatLng(37.77843, -122.41942),
+              zoom: 12,
+            ),
+          ),
+          Positioned(
+            bottom: 10.0,
+            child: SafeArea(
+              child: Container(
+                height: 200.0,
+                alignment: Alignment.bottomCenter,
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 32),
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: items.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      width: 300.0,
+                      child: Card(
+                        child: ListTile(
+                          title: Text(
+                            'Item $index',
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
