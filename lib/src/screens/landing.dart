49a086d9b1bbb3eca826ea1e016142fcbc3a0d02
Osman Moomin <o.abdulmoomin@gmail.com>
2020-06-27 20:46:25 +0100

    integrate flare animation

--- lib/src/screens/landing.dart
+++ lib/src/screens/landing.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
-
+import 'package:flare_flutter/flare_actor.dart';
 import 'map_screen.dart';
 
-class LandingScreen extends StatelessWidget {
+class LandingScreen extends StatefulWidget {
   static const String id = '/landing_screen';
+
+  _LandingScreenState createState() => _LandingScreenState();
+}
+
+class _LandingScreenState extends State<LandingScreen> {
+  bool isOpen = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: FlatButton(
-          onPressed: () {
-            Navigator.pushNamed(context, MapScreen.id);
-          },
-          child: Text('Jump in.'),
-          color: Theme.of(context).accentColor,
+      body: GestureDetector(
+        onTap: () {
+          Navigator.pushNamed(context, MapScreen.id);
+        },
+        child: FlareActor(
+          'assets/ui.flr',
+          animation: 'show',
         ),
       ),
     );
