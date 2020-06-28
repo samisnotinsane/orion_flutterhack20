70c8e2e2e4e5600c2e6ad604f0443797fb64cead
cakeydoodoo1 <tienvu1812@gmail.com>
2020-06-28 16:02:53 +0100

    fix card size

--- lib/src/widgets/report_card.dart
+++ lib/src/widgets/report_card.dart
@@ -8,68 +8,69 @@ class ReportCard extends StatelessWidget {
   final Report report;
 
   ReportCard(this.report);
-
   Widget build(context) {
-    return AspectRatio(
-      aspectRatio: 13 / 15,
-      child: Card(
-        color: Styles.primaryColorAlt,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Expanded(
-              flex: 2,
-              child: ClipRect(
-                child: FittedBox(
-                  child: Image.asset(report.imagePath,),
-                  fit: BoxFit.fill
-                ),
+    return Card(
+      color: Styles.primaryColorAlt,
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 2,
+            child: AspectRatio(
+              aspectRatio:3/2,
+              child: Image.asset(report.imagePath, fit: BoxFit.fill)),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: <Widget>[
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        report.title,
+                        style: Styles.reportCardTitle,
+                      ),
+                      SizedBox(height: 8.0),
+                      Row(
+                        children: <Widget>[
+                          Text(
+                            '${report.karma}',
+                            style: Styles.reportCardSynopsis,
+                          ),
+                          SizedBox(width: 5.0),
+                          Text('karma'),
+                        ],
+                      ),
+                      Text(
+                        '${report.minsPassed} mins ago',
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: 50.0,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[],
+                      ),
+                    ],
+                  ),
+                  RetroButton(
+                    onPressed: () {
+                      // TODO: Nav to details.
+                      print('Card onPressed not yet implemented');
+                    },
+                    title: 'Tidy',
+                  ),
+                ],
               ),
             ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  children: <Widget>[
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          report.title,
-                          style: Styles.reportCardTitle,
-                        ),
-                        Row(
-                          children: <Widget>[
-                            Text(
-                              '${report.karma}',
-                              style: Styles.reportCardSynopsis,
-                            ),
-                            SizedBox(width: 5.0),
-                            Text('karma'),
-                          ],
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: <Widget>[
-                        IconButton(
-                          icon: Icon(Icons.explore),
-                          onPressed: () {
-                            // TODO: Nav to details.
-                            print('Card onPressed not yet implemented');
-                          },
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
