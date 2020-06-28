fb2b48c0edf65c8fc7c0498033f18edd8082c3e1
ADBurpitt <aaronburpitt@gmail.com>
2020-06-28 18:24:04 +0100

    Add QR code dialog

--- lib/src/widgets/partner_list_tile.dart
+++ lib/src/widgets/partner_list_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 import '../models/partner.dart';
 import './retro_button.dart';
@@ -8,6 +9,26 @@ class PartnerListTile extends StatelessWidget {
   final int karma;
 
   PartnerListTile({this.partner, this.karma});
+
+  Future<void> claimerino(context) {
+    showDialog(
+        context: context,
+        builder: (context) => SimpleDialog(
+              title: Text("Enjoy!"),
+              children: [
+                Center(
+                    child: Container(
+                        margin: EdgeInsets.only(bottom: 10),
+                        width: 100,
+                        height: 100,
+                        child: QrImage(
+                          data: partner.id,
+                          version: QrVersions.auto,
+                          size: 200.0,
+                        ))),
+              ],
+            ));
+  }
 
   Widget build(context) => Container(
         margin: EdgeInsets.symmetric(vertical: 5),
@@ -24,7 +45,8 @@ class PartnerListTile extends StatelessWidget {
                 ]),
             isThreeLine: true,
             trailing: partner.cost < karma
-                ? RetroButton(title: "CLAIM", onPressed: null)
+                ? RetroButton(
+                    title: "CLAIM", onPressed: () => claimerino(context))
                 : null),
       );
 }
